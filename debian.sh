bb39671c2f7f1dcae9ab9ad9e3b7a0dd492c4d61
Matheus A.M <matheusthebr@gmail.com>
2019-10-11 19:46:34 -0300

    Add spotify.

--- debian.sh
+++ debian.sh
@@ -47,6 +47,11 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# SPOTIFY
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
 ## CC_DOTFILES SETUP
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
