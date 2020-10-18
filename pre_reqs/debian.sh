8af8c25fa78f9f93edfbe12b86eb1accd5732310
Matheus A.M <matheus.martins@qflash.com.br>
2020-10-18 16:10:25 -0300

    Organized prereqs scripts.

--- pre_reqs/debian.sh
+++ pre_reqs/debian.sh
@@ -1,54 +1,80 @@
 #!/bin/sh
 
-# DOCKER - DEBIAN
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
-# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-# sudo add-apt-repository \
-#    "deb [arch=amd64] https://download.docker.com/linux/debian \
-#    $(lsb_release -cs) \
-#    stable"
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
+install_docker() {
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+  sudo apt-get update
+  sudo apt-get install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg2 \
+      software-properties-common
 
-#DOCKER-COMPOSE
-sudo apt-get remove docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
 
-# SPOTIFY
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
 
-## dotfiles SETUP
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install -y silversearcher-ag \
- zsh \
- tmux \
- dconf-cli
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+  sudo docker run hello-world
+}
 
-git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
-~/.dotfiles/gnome-terminal-colors-solarized/install.sh
-sudo apt-get purge ruby
-echo 'Change your terminal window to Run command as login shell and restart'
-echo 'You can find more information about this on' \
-     'https://github.com/rvm/ubuntu_rvm'
+install_docker_compose() {
+  sudo apt-get remove docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
+
+install_spotify() {
+  curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+  sudo apt-get update && sudo apt-get install spotify-client
+}
+
+install_tmux() {
+  TMUX_VERSION="3.1b"
+  TMUX_SOURCE_FILE="tmux-${TMUX_VERSION}.tar.gz"
+  TMUX_SOURCE_FOLDER="tmux-${TMUX_VERSION}"
+
+  echo "Installing tmux ${TMUX_VERSION}"
+  wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/${TMUX_SOURCE_FILE}
+  tar -xf ${TMUX_SOURCE_FILE}
+  pushd $TMUX_SOURCE_FOLDER
+  ./configure
+  make
+  sudo make install
+  popd
+  rm -rf $TMUX_SOURCE_FOLDER
+  rm $TMUX_SOURCE_FILE
+}
+
+install_gnome_terminal_colors() {
+  GIT_REPO="https://github.com/Anthony25/gnome-terminal-colors-solarized.git"
+  COLORS_PATH="$HOME/.gnome-terminal-colors-solarized"
+
+  [ -d $COLORS_PATH ] && return
+
+  git clone $GIT_REPO $COLORS_PATH
+  $COLORS_PATH/install.sh
+}
+
+echo 'Starting debian install'
 
 sudo apt-get upgrade && apt-get update
+
+install_docker
+install_docker_compose
+install_spotify
+install_tmux > /dev/null 2>&1
+install_gnome_terminal_colors
+
+sudo apt-get remove -y ruby
+sudo apt-get autoremove -y
+
 echo 'Minimal requests installed! =]'
--- pre_reqs/ubuntu.sh
+++ pre_reqs/ubuntu.sh
@@ -1,4 +1,40 @@
-#!/bin/bash
+#!/bin/sh
+
+install_docker() {
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+  sudo apt-get update
+  sudo apt-get install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg2 \
+      software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+  sudo docker run hello-world
+}
+
+install_docker_compose() {
+  sudo apt-get remove docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
+
+install_spotify() {
+  curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+  sudo apt-get update && sudo apt-get install spotify-client
+}
 
 install_tmux() {
   TMUX_VERSION="3.1b"
@@ -27,8 +63,17 @@ install_gnome_terminal_colors() {
   $COLORS_PATH/install.sh
 }
 
+echo 'Starting ubuntu install'
+
+sudo apt-get upgrade && apt-get update
+
+install_docker
+install_docker_compose
+install_spotify
 install_tmux > /dev/null 2>&1
 install_gnome_terminal_colors
 
 sudo apt-get remove -y ruby
 sudo apt-get autoremove -y
+
+echo 'Minimal requests installed! =]'
