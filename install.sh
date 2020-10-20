b0df7c36c017816c5107c2a24130ddbda621676c
Matheus A.M <matheus.martins@qflash.com.br>
2020-10-20 11:42:24 -0300

    Removed git before building it.

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ rbenv_install() {
 }
 
 git_install() {
+  sudo apt remove git
   sudo apt install make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
   cd /usr/src/
   sudo wget https://github.com/git/git/archive/v2.29.0.tar.gz -O git.tar.gz
