44932a15a5aa32fc73e78ef9e15aaa1c990a959f
Harry <matheusthebr@gmail.com>
2021-10-06 20:00:57 -0300

    Fixed docker setup.

--- pre_reqs/debian.sh
+++ pre_reqs/debian.sh
@@ -26,9 +26,11 @@ install_docker() {
 
 install_docker_compose() {
   sudo apt-get remove docker-compose
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  which docker-compose
+  docker-compose -v
 }
 
 install_spotify() {
