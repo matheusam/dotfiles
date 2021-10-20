8c5214df190290d7e5bee8c002b009b1d3268e8a
Vini Machado <vinimachadosantana@gmail.com>
2021-10-20 19:58:24 -0300

    Sudo it up

--- pre_reqs/ubuntu.sh
+++ pre_reqs/ubuntu.sh
@@ -150,9 +150,9 @@ install_fira_code_font() {
 }
 
 install_erlang_and_elixir() {
-  apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
-  asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-  asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+  sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
+  sudo asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+  sudo asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 }
 
 echo 'Starting ubuntu install'
