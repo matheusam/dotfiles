1082bd533caa0b8c27e767fb45f36aaecce69815
Vini Machado <vinimachadosantana@gmail.com>
2021-10-27 10:35:42 -0300

    Refac debian elixir installation

--- pre_reqs/debian.sh
+++ pre_reqs/debian.sh
@@ -170,9 +170,8 @@ install_fira_code_font() {
 }
 
 install_erlang_and_elixir() {
-  sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
-  sudo asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-  sudo asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+  asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+  asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
 }
 
 echo 'Starting debian install'
